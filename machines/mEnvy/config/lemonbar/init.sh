c5cdfdeb2ff0d12be9fd432a1587b98455dd3187
Marten <marten143@gmail.com>
2018-09-16 12:02:19 +0200

    fixed some lemonbar highlighting glitches

--- machines/mEnvy/config/lemonbar/init.sh
+++ machines/mEnvy/config/lemonbar/init.sh
@@ -6,13 +6,15 @@ source "$HOME/.scripts/colors.sh"
 xoff=0
 yoff=0
 width=$(xrandr | awk '{if ($3=="primary") print $4}' | awk 'BEGIN{FS="x"}{print $1}')
-height=35
+height=40
 
 # Fonts
-font1="Knack Nerd Font Mono:size=9"
+font1="Hack:size=10"
+font2="Knack Nerd Font Mono:size=16"
+#font1="Hack:size=9"
 # font2="-wuncon-siji-medium-r-normal--10-100-75-75-c-80-iso10646-1"
 
-sh $HOME/.config/lemonbar/topbar.sh | lemonbar -f "$font1" -F $foreground -B $background -g "${width}x${height}+$xoff+$yoff" -o 1 -u 2 | while read action; do
+sh $HOME/.config/lemonbar/topbar.sh | lemonbar -f "$font1" -o -8 -f "$font2" -o -1 -F $foreground -B $background -g "${width}x${height}+$xoff+$yoff" -o 1 -u 2 | while read action; do
     case "$action" in
 
     "mute")
--- machines/mEnvy/config/lemonbar/topbar.sh
+++ machines/mEnvy/config/lemonbar/topbar.sh
@@ -2,8 +2,11 @@
 
 # Colors
 source "$HOME/.scripts/colors.sh"
-accent="%{F$highlights}"
+# accent="%{F$highlights}"
+accent="%{F$foreground}"
 text="%{F$foreground}"
+
+interface=$(ip link | grep -Po '2: \K(.*)(?=:)')
 
 
 clock() {
@@ -23,16 +26,16 @@ vol() {
     off=$(amixer get Master | grep 'off')
 
     if [ -z "$off" ]; then
-        echo -e "$accent\uf028$text $level"
+        echo -e "\uf028 $level"
     else
-        echo -e "$accent\uf026$text m"
+        echo -e "\uf026 m"
     fi
 }
 
 mem() {
     mkb=$(cat /proc/meminfo | awk '/Active:/{print $2}')
     mmb=${mkb:0:-3}
-    echo -e "$accent\uf472$text ${mmb}M"
+    echo -e "\uf472 ${mmb}M"
 }
 
 song() {
@@ -41,7 +44,7 @@ song() {
     title=$(playerctl metadata title 2>/dev/null)
 
     if [ ! -z $playing ]; then
-        echo -e "|  $accent\u25b6$text $artist - $title"
+        echo -e "$accent\u25b6$text $artist - $title"
     else
         echo ''
     fi
@@ -49,7 +52,7 @@ song() {
 
 i3() {
     ws=$(i3-msg -t get_workspaces)
-    out="%{U$highlights}"
+    out="%{U$background}"
 
     for i in $1; do
         t=$(grep -o -P "\"num\":$i.*?(\"rect\")" <<< $ws)
@@ -58,29 +61,31 @@ i3() {
             v=$(grep -o '"visible":true' <<< $t)
             if [ ! -z $v ]; then
                 # Workspace is visible
-                out="$out$accent%{!u} $i %{!u}$text"
+                # out="$out$accent%{!u} $i %{!u}$text"
+                out="$out %{!u}\u25cf%{!u}"
             else
                 # Workspace not visible
-                out="$out $i "
+                # out="$out $i "
+                out="$out \u25cf"
             fi
-        # else
-        #     # Workspace empty
-        #     out="$out \u25c7 "
+        else
+            # Workspace empty
+            out="$out \u25cb"
         fi
     done
 
     out="$out%{U-}"
-    echo -e "$out"
+    echo -e "$out$text"
 }
 
 wifi() {
-    ssid=$(iw dev wlp1s0 link | grep "SSID:" | cut -d' ' -f2)
+    ssid=$(iw dev $interface link | grep -Po "SSID: \K.*$")
 
-    if [ -z $ssid ]; then
+    if [ -z "$ssid" ]; then
         ssid="N/A"
     fi
 
-    echo -e "$accent\uf012$text $ssid"
+    echo -e "\uf012 $ssid"
 }
 
 battery() {
@@ -90,24 +95,22 @@ battery() {
     icons="\uf244 \uf243 \uf242 \uf241 \uf240"
     icount=$(wc -w <<< $icons)
     
-    # perc=50
     num=$(awk -v perc=$perc -v icount=$icount 'BEGIN {printf "%1.0f", perc/100*(icount-1)+1}')
 
-    if [ ! -z $charging ]; then
+    if [ ! -z "$charging" ]; then
         # Charging
-        icon='\uf0e7'
+        icon='\uf1e6'
     else
         # Discharging
         icon=$(cut -d' ' -f$num <<< $icons)
     fi
-    echo -e "$accent$icon$text $perc%"
+    echo -e "$icon $perc%"
 }
 
 
 while true; do
-    # i3l="$(i3 '1 2 3 4 5')"
     i3l="$(i3 '1 2 3 4 5 6 7 8 9 0')"
 
-    echo -e "%{l}    $i3l  |  %{A:mute:}$(vol)%{A}  $(song) %{c}$(cal), $(clock) %{r}$(battery)  $(wifi)  $(mem)    "
+    echo -e "%{l}%{R} $i3l  %{B-}%{F-}  $(song) %{c}$(cal), $(clock) %{r}$(wifi)  %{R}  %{A:mute:}$(vol)%{A}  $(mem)  $(battery)  %{B-}%{F-}"
     sleep 0.1
 done
